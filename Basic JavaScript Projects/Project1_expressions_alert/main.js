a24192a9945e88700e93443dcf7007945006d6ce
Ozan <ozannsariaslan@gmail.com>
2024-02-29 20:57:03 +0000

    added 6 requirments for module 1

--- Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,1 +1,17 @@
-window.alert("Hello World!");
+document.write("Hello World!"); //write on screen
+
+window.alert("Hello, World"); //sent notification screen
+
+var A="This is a variable 1"; //variable A
+var D="This is variable 2"   //variable D
+document.write(A+D); //concatenate var a+d
+
+
+document.write("Lisa told Bart, \"Hey!\"");
+document.write(`\"Learn the rules like a \"pro\",so you can break them like an \"artist\"\.-Pablo Picasso-`);
+
+
+var family="Adams", Dad="James", Mom="Jenny", Son="Harry", Dog="Jerry"; //Comment
+document.write(family);
+
+document.write(6+6); //expression
