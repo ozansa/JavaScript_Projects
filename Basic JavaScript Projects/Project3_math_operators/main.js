ce352636f845facff902e8c4643045eeaf3bab8c
Ozan <ozannsariaslan@gmail.com>
2024-03-05 15:54:29 +0000

    Added subtraction

--- Basic JavaScript Projects/Project3_math_operators/main.js
+++ Basic JavaScript Projects/Project3_math_operators/main.js
@@ -2,3 +2,8 @@ function subtraction_Function(){
     var Subtraction=5-2;
     document.getElementById("sub").innerHTML="5-2=" + Subtraction;
 }
+
+function addition_Function(){
+    let addition=2+2;
+document.getElementById("add").innerHTML="2+2=" +addition;
+}
