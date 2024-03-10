7af9ca30915dd4277a93987ef03f887f1de507a1
Ozan <ozannsariaslan@gmail.com>
2024-03-10 14:26:40 +0000

    Added tictactoe html code

--- Basic JavaScript Projects/Project10_loops_arrays/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/main.js
@@ -31,3 +31,25 @@ function cat_pics(){
     document.getElementById("Cat").innerHTML="In this picture, the cat is" +
     Cat_Picture[2] + ".";
 }
+
+//const function
+function constant_function(){
+    const Musical_Instrument={type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color="blue";
+    Musical_Instrument.price="900$"
+    document.getElementById("Constant").innerHTML="The cost of the "+ Musical_Instrument.type + "was " + Musical_Instrument.price;
+}
+
+//objects
+
+let car= {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color:"red ",
+    description: function (){
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML=car.description();
+
